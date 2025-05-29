78ebc1179e85a91b2c663339e13f37e5237437ed
minjung <minjung0768@gmail.com>
2025-05-29 18:14:18 +0900

    style(application): 불필요한 공백 줄 제거

--- src/main/java/project/vmo/VmoApplication.java
+++ src/main/java/project/vmo/VmoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VmoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VmoApplication.class, args);
     }
-
 }
