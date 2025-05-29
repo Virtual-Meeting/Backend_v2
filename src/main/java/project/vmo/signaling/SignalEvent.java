193670d3dce835bd85dc103340a63f12262ac933
minjung <minjung0768@gmail.com>
2025-05-29 16:50:23 +0900

    feat(signal event): enum 추가 및 문자열 값으로부터 이벤트 매핑 기능 구현
    - createRoom 이벤트 추가
    - 유효하지 않은 이벤트 처리 시 IllegalArgumentException 발생

---
+++ src/main/java/project/vmo/signaling/SignalEvent.java
@@ -0,0 +1,23 @@
+package project.vmo.signaling;
+
+import lombok.Getter;
+
+@Getter
+public enum SignalEvent {
+    CREATE_ROOM("createRoom");
+
+    private final String value;
+
+    SignalEvent(String value) {
+        this.value = value;
+    }
+
+    public static SignalEvent from(String value) {
+        for (SignalEvent event : values()) {
+            if (event.getValue().equals(value)) {
+                return event;
+            }
+        }
+        throw new IllegalArgumentException("유효하지 않은 이벤트입니다: event id = " + value);
+    }
+}
