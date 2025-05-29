c711d61f71271b6a8dfc376dabd73586f37c25a9
minjung <minjung0768@gmail.com>
2025-05-29 16:57:42 +0900

    feat(user session factory): UserSession 생성 로직 구현
    - IceCandidateService 주입 및 활용
    - MediaPipeline 기반 WebRtcEndpoint 생성 후 UserSession 반환

---
+++ src/main/java/project/vmo/session/UserSessionFactory.java
@@ -0,0 +1,26 @@
+package project.vmo.session;
+
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.WebRtcEndpoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+import project.vmo.Service.IceCandidateService;
+import project.vmo.domain.UserSession;
+
+@Component
+public class UserSessionFactory {
+    private final IceCandidateService iceCandidateService;
+
+    public UserSessionFactory(IceCandidateService iceCandidateService) {
+        this.iceCandidateService = iceCandidateService;
+    }
+
+    public UserSession create(String username, String roomId,
+                              Boolean isAudioOn, Boolean isVideoOn,
+                              WebSocketSession session, MediaPipeline pipeline) {
+
+        WebRtcEndpoint outgoing = new WebRtcEndpoint.Builder(pipeline).build();
+
+        return new UserSession(username, roomId, isAudioOn, isVideoOn, session, pipeline, outgoing, iceCandidateService);
+    }
+}
