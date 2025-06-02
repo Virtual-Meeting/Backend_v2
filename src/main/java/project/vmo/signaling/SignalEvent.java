5bec6221f59ebd920158e10f14b6246882e1c5be
minjung <minjung0768@gmail.com>
2025-06-02 10:34:08 +0900

    feat(signal event): 클라이언트 요청에 대응할 enum 항목 추가
    - 클라이언트에서 특정 요청을 보내는 상황에서 서버가 해당 요청을 인식하지 못해 발생하는 에러를 방지하기 위해,
    아직 구현되지 않은 요청 타입을 enum에 추가함

--- src/main/java/project/vmo/signaling/SignalEvent.java
+++ src/main/java/project/vmo/signaling/SignalEvent.java
@@ -6,11 +6,23 @@ import lombok.Getter;
 public enum SignalEvent {
     CREATE_ROOM("createRoom"),
     JOIN_ROOM("joinRoom"),
+    EXIT_ROOM("exitRoom"),
     ICE_CANDIDATE("onIceCandidate"),
     RECEIVE_VIDEO("receiveVideoAnswer"),
     SEND_EMOJI("sendPublicEmoji"),
     BROADCAST_CHAT("broadcastChat"),
-    SEND_PERSONAL_CHAT("sendPersonalChat");
+    SEND_PERSONAL_CHAT("sendPersonalChat"),
+    CHANGE_NAME("changeName"),
+    AUDIO_STATE_CHANGE("audioStateChange"),
+    VIDEO_STATE_CHANGE("videoStateChange"),
+    START_RECORDING("startRecording"),
+    STOP_RECORDING("stopRecording"),
+    SAVE_RECORDING("saveRecording"),
+    PAUSE_RECORDING("pauseRecording"),
+    RESUME_RECORDING("resumeRecording"),
+    REQUEST_RECORDING_PERMISSION("requestRecordingPermission"),
+    GRANT_RECORDING_PERMISSION("grantRecordingPermission"),
+    DENY_RECORDING_PERMISSION("denyRecordingPermission");
 
     private final String value;
 
