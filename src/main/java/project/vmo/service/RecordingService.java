79b8273ccd2a6e80a28df03cc2cb3e96bf46eb62
minjung <minjung0768@gmail.com>
2025-06-10 18:35:51 +0900

    feat(recording service): 일반 사용자의 녹화 권환 확인, 허락, 삭제 로직 추가

--- src/main/java/project/vmo/service/RecordingService.java
+++ src/main/java/project/vmo/service/RecordingService.java
@@ -9,11 +9,18 @@ import project.vmo.domain.UserSession;
 import project.vmo.signaling.SignalEvent;
 import project.vmo.util.MessageCreator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class RecordingService {
     private static final Logger log = LoggerFactory.getLogger(RecordingService.class);
 
     private final RoomService roomService;
+
+    private final Map<String, String> permittedUser = new HashMap<>();
 
     public RecordingService(RoomService roomService) {
         this.roomService = roomService;
@@ -50,7 +57,7 @@ public class RecordingService {
             SendService.sendMessage(userSession.getSession(), MessageCreator.createStopRecordingMessage(SignalEvent.STOP_RECORDING.getValue(), recorder.getName()));
         }
 
-        deleteRecordingPermission(userSession);
+        deleteRecordingPermission(userSession.getSession().getId());
     }
 
     public void pauseRecording(UserSession userSession) {
@@ -65,6 +72,31 @@ public class RecordingService {
         SendService.sendMessage(userSession.getSession(), MessageCreator.simple(SignalEvent.PAUSE_RECORDING.getValue()));
     }
 
+    public void grantRecordingPermission(UserSession userSession) {
+        String sessionId = userSession.getSession().getId();
+        String roomId = userSession.getRoomId();
+
+        validateRecordPermission(sessionId, roomId);
+
+        permittedUser.put(sessionId, roomId);
+        SendService.sendMessage(userSession.getSession(), MessageCreator.createGrantPermissionMessage(sessionId));
+    }
+
+    public void validateRecordPermission(String sessionId, String roomId) {
+        Room room = roomService.getRoomById(roomId);
+        String roomLeaderId = room.getLeaderSessionId();
+
+        if (permittedUser.containsKey(sessionId) || sessionId.equals(roomLeaderId)) {
+            throw new IllegalStateException("이미 녹화 권한이 부여된 사용자입니다.");
+        }
+
+        long count = permittedUser.values().stream()
+                .filter(id -> id.equals(roomId))
+                .count();
+
+        if (count >= 2) throw new IllegalStateException("녹화 권한은 최대 2명까지 부여할 수 있으며, 현재 이미 2명이 권한을 보유하고 있습니다.");
+    }
+
     private void checkRecordingPermission(UserSession userSession) {
         Room room = roomService.getRoomById(userSession.getRoomId());
         String roomLeaderId = room.getLeaderSessionId();
@@ -74,11 +106,12 @@ public class RecordingService {
         }
     }
 
-    private void deleteRecordingPermission(UserSession userSession) {
-
+    private void deleteRecordingPermission(String sessionId) {
+        permittedUser.remove(sessionId);
     }
 
     private boolean isPermittedUser(UserSession userSession) {
-        return false;
+        Room room = roomService.getRoomById(userSession.getRoomId());
+        return room.getLeaderSessionId().equals(userSession.getSession().getId()) || permittedUser.containsKey(userSession.getSession().getId());
     }
 }
